1a8da2b0924b3bee5ce3b42456ada63e7940c633
Spencer Imbleau <spencer@imbleau.com>
2021-05-24 09:52:22 -0400

    Fixed linebreak in documentation

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -2,8 +2,8 @@ use nvtx_rs::{range_pop, range_push};
 use std::thread::sleep;
 use std::time::Duration;
 
-/// Push a range on the NVTX layer which gets popped after
-/// an expensive operation. Try running this example from NVIDIA Nsight Systems!
+/// Push a range on the NVTX layer which gets popped after an expensive operation.
+/// Try running this example from NVIDIA Nsight Systems!
 fn main() {
     range_push("Hello, World!");
     sleep(Duration::from_secs(1)); // Expensive operation here
