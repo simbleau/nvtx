e9e16a5654ebcf773046b7f0e125e6c534f5c13b
Spencer Imbleau <spencer@imbleau.com>
2022-05-29 11:02:45 -0400

    fix: Build now triggers on export.c changes

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
 /// Build script to compile the exported functions for NVIDIA® Tools Extension SDK (NVTX).
 fn main() {
     // Compile the exported functions for NVIDIA® Tools Extension SDK (NVTX).
-    // This will fail if you do not have the shared headers shipped with
-    // CUDA Toolkit and Nsight tools such as Nsight Graphics and Nsight Systems.
+    // Ensure submodules are initialized.
     cc::Build::new().file("nvtx-sys/export.c").compile("nvtx");
 
     // Recompile only if the exported function definitions change.
-    println!("cargo:rerun-if-changed=src/nvtx.c");
+    println!("cargo:rerun-if-changed=nvtx-sys/export.c");
 }
